3535a98e22ee4a26ae653d8c5102dc9244302f06
Olga Ivanova <olga.ivanova@ambient.digital>
2022-06-20 15:08:42 +0200

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import styled from 'styled-components';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+
+function Header() {
+  return (
+    <Wrapper>
+      <LogoWrapper>
+        <PinterestIcon />
+      </LogoWrapper>
+      <HomePageButton>
+        <a href='/'>Homepage</a>
+      </HomePageButton>
+      <FollowingButton>
+        <a href='/'>Following</a>
+      </FollowingButton>
+      {/* <SearchWrapper>
+        <SearchBarWrapper></SearchBarWrapper>
+      </SearchWrapper>
+      <IconsWrapper></IconsWrapper> */}
+    </Wrapper>
+  );
+}
+
+export default Header;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 56px;
+  padding: 12px 4px 4px 16px;
+  background-color: white;
+  color: black;
+`;
+
+const LogoWrapper = styled.div`
+  .MuiSvgIcon-root {
+    color: #e60023;
+    font-size: 32px;
+    cursor: pointer;
+  }
+`;
+
+const HomeButtons = styled.div`
+  display: flex;
+  height: 48px;
+  min-width: 123px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 24px;
+  cursor: pointer;
+`;
+
+const HomePageButton = styled(HomeButtons)`
+  background-color: rgb(17, 17, 17);
+
+  a {
+    text-decoration: none;
+    color: white;
+    font-weight: 700;
+  }
+`;
+
+const FollowingButton = styled(HomeButtons)`
+  background-color: white;
+
+  a {
+    text-decoration: none;
+    color: rgb(17, 17, 17);
+    font-weight: 700;
+  }
+
+  :hover {
+    background-color: #e1e1e1;
+  }
+`;
